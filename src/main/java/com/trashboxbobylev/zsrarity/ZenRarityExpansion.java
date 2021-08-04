fda8f113a8177eee46ee89b62eade942206a9de8
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-08-05 00:12:03 +0300

    fully implemented botania support

--- src/main/java/com/trashboxbobylev/zsrarity/ZenRarityExpansion.java
+++ src/main/java/com/trashboxbobylev/zsrarity/ZenRarityExpansion.java
@@ -1,10 +1,16 @@
 package com.trashboxbobylev.zsrarity;
 
+import crafttweaker.CraftTweakerAPI;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.item.IItemStack;
 import crafttweaker.api.minecraft.CraftTweakerMC;
+import crafttweaker.mc1120.actions.ActionSetStackTranslation;
+import crafttweaker.mc1120.game.MCGame;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.translation.I18n;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import stanhebben.zenscript.annotations.ZenExpansion;
 import stanhebben.zenscript.annotations.ZenMethod;
 
@@ -13,13 +19,24 @@ import stanhebben.zenscript.annotations.ZenMethod;
 public class ZenRarityExpansion {
     @ZenMethod
     public static void setRarity(IItemStack itemStack, String colorName){
-        String ogName = I18n.translateToLocal(CraftTweakerMC.getItemStack(itemStack).getItem().getUnlocalizedNameInefficiently(
+        String key = CraftTweakerMC.getItemStack(itemStack).getItem().getUnlocalizedNameInefficiently(
                 CraftTweakerMC.getItemStack(itemStack)
-        ) + ".name");
-        if (ogName.startsWith("\u00A7") && ogName.substring(1).matches("[0-9a-f]")) {
-            itemStack.setDisplayName(ogName.substring(1).replaceFirst("[0-9a-f]", ""));
+        ) + ".name";
+        String key2 = CraftTweakerMC.getItemStack(itemStack).getUnlocalizedName();
+        if (key2.contains("specialflower")){
+            key = key2;
         }
+        String ogName = I18n.translateToLocal( key + ".name");
 
-        itemStack.setDisplayName(TextFormatting.getValueByName(colorName) + itemStack.getDisplayName());
+        if (ogName.startsWith("\u00A7") && ogName.substring(1).matches("[0-9a-f]")) {
+            setDisplayName(key, ogName.substring(1).replaceFirst("[0-9a-f]", ""));
+        }
+        setDisplayName(key, TextFormatting.getValueByName(colorName) + itemStack.getDisplayName());
+    }
+
+    private static void setDisplayName(String key, String value){
+        ActionSetStackTranslation action = new ActionSetStackTranslation(null, key, value);
+        MCGame.TRANSLATION_ACTIONS.add(action);
+        CraftTweakerAPI.apply(action);
     }
 }
