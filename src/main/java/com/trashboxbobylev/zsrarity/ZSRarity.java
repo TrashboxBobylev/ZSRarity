f0c2da7196ea49879196606e3ebdff74367d87bf
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-08-04 17:38:59 +0300

    reworked entire mod, added runtime changing

--- src/main/java/com/trashboxbobylev/zsrarity/ZSRarity.java
+++ src/main/java/com/trashboxbobylev/zsrarity/ZSRarity.java
@@ -1,12 +1,6 @@
 package com.trashboxbobylev.zsrarity;
 
-import net.minecraft.init.Blocks;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = ZSRarity.MODID, name = ZSRarity.NAME, version = ZSRarity.VERSION, dependencies = "required-after:crafttweaker")
 public class ZSRarity
@@ -14,9 +8,4 @@ public class ZSRarity
     public static final String MODID = "zsrarity";
     public static final String NAME = "ZSRarity";
     public static final String VERSION = "1.0";
-
-    @Mod.EventHandler
-    public static void preinit(FMLPreInitializationEvent e) {
-        MinecraftForge.EVENT_BUS.register(RarityHandler.class);
-    }
 }
--- src/main/java/com/trashboxbobylev/zsrarity/ZenRarityExpansion.java
+++ src/main/java/com/trashboxbobylev/zsrarity/ZenRarityExpansion.java
@@ -4,23 +4,24 @@ import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.item.IItemStack;
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import crafttweaker.mc1120.CraftTweaker;
-import net.minecraft.item.Item;
 import net.minecraft.util.text.TextFormatting;
-import stanhebben.zenscript.annotations.ZenClass;
+import net.minecraft.util.text.translation.I18n;
 import stanhebben.zenscript.annotations.ZenExpansion;
 import stanhebben.zenscript.annotations.ZenMethod;
-
-import java.util.HashMap;
 
 @ZenRegister
 @ZenExpansion("crafttweaker.item.IItemStack")
 public class ZenRarityExpansion {
-    public static HashMap<Item, TextFormatting> customRarities = new HashMap<>();
-
     @ZenMethod
     public static void setRarity(IItemStack itemStack, String colorName){
-        customRarities.put(CraftTweakerMC.getItemStack(itemStack).getItem(), TextFormatting.getValueByName(colorName));
-        CraftTweaker.LOG.info(CraftTweakerMC.getItemStack(itemStack).getItem().toString() +
-                " " + TextFormatting.getValueByName(colorName).toString());
+        String ogName = I18n.translateToLocal(CraftTweakerMC.getItemStack(itemStack).getItem().getUnlocalizedNameInefficiently(
+                CraftTweakerMC.getItemStack(itemStack)
+        ) + ".name");
+        if (ogName.startsWith("\u00A7") ) {
+            CraftTweaker.LOG.info(ogName.substring(2));
+            itemStack.setDisplayName(ogName.substring(2));
+        }
+
+        itemStack.setDisplayName(TextFormatting.getValueByName(colorName) + itemStack.getDisplayName());
     }
 }
